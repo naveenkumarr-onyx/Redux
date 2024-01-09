191c5d57918f4257a22ab0fc821a0a55e26caeda
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-01-09 19:21:37 +0530

    Linked netlify

--- src/slices/tasksSlice.js
+++ src/slices/tasksSlice.js
@@ -5,7 +5,7 @@ const initialState = {
   isLoading: false,
   error: "",
 };
-const BASE_URL = "http://localhost:4000/api/tasks"
+const BASE_URL = "https://thunderous-puppy-767a70.netlify.app/"
 // get
 export const getTasksFromServer = createAsyncThunk(
   "tasks/getTasksFromServer",
